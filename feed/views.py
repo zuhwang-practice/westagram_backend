ed3ee06c1c0a4677460369e9d26a31a599ccc844
zu-hwang <tropical.kitsch@gmail.com>
2020-04-15 20:01:49 +0900

    wip

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,5 @@
+import json # req.body에 받는 정보 변환
+
 # from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse, JsonResponse
@@ -10,11 +12,9 @@ from .models import Comment, Feed, FeedComment
 
 
 class Feed(View):
-    
-    @login_required
+    # @login_required
     def get(self, req):
         print(req)
-      
         # 데이터베이스에서 피드 가져와서 화면에 뿌릴 피드 객체만들기
         # 로그인이 됬는지 확인, 유저정보 확인!
         # 각각의 DB에서 
@@ -23,6 +23,7 @@ class Feed(View):
         return JsonResponse({'message': '응답!'})
 
     def post(self, req):
+        
         data = req.body # 바디 정보 담기
         return JsonResponse({'message': 'DB update'})
       
@@ -31,7 +32,6 @@ class Feed(View):
     #   get으로 해야하는 건지 post로 해야 하는건지..? 
     def getFeedList(self):
         get_feed_list = Feed.get();
-        User.
         print(get_feed_list)
         result = {
             # data안에 리스트>각 인덱스에 DB필드 &값 보내기
@@ -51,8 +51,17 @@ class Feed(View):
                        {
                         'user_id':'',
                         'conmment_text':''}
-                      ],~
+                      ],
                 }
             ]
         }
         return result
+      
+      
+      
+class CommentView():
+    # 뿌려줄 일은 없고, 받으면 comment 테이블에 데이터는 저장해야함.
+    def post(self, req):
+        token = req.headers.get('Authorization', None) # 해더에서 담긴 토큰 꺼내기
+        data  = json.loads(req.body) # 바디 데이터 꺼내기
+        
